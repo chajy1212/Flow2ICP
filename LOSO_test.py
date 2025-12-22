94e95d4a6c485853887dcb94c00882bcdfbec10f
chajy1212 <chajy1212@naver.com>
2025-12-22 13:07:35 +0900

    #

--- LOSO_test.py
+++ LOSO_test.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import torch
 from torch import nn
-from sklearn.preprocessing import RobustScaler
 import matplotlib.pyplot as plt, random
 import os, glob
 from PIL import Image
